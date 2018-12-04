0229610e0db4990acc8b887a7cf87af42a37cd2a
Mark Daoust <markdaoust@google.com>
2018-12-04 09:35:45 -0800

    Sync from github
    PiperOrigin-RevId: 223992181

--- site/en/2/README.md
+++
@@ -1,3 +0,0 @@
-# TensorFlow 2.0 updates
-
-Read the announcement: [TensorFlow 2.0 is coming](https://groups.google.com/a/tensorflow.org/d/msg/announce/qXfsxr2sF-0/jHQ77dr3DAAJ)
--- tools/tensorflow_docs/api_generator/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Tools for building tensorflow api reference docs."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import tensorflow_docs.api_generator.doc_controls
-import tensorflow_docs.api_generator.generate_lib
