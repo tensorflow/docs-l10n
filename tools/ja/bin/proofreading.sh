934135c55a32e9dc06d7a14bfaa9947970f38610
sfujiwara <shuhei.fujiwara@gmail.com>
2020-05-17 16:03:08 +0900

    write log file

--- tools/ja/bin/proofreading.sh
+++ tools/ja/bin/proofreading.sh
@@ -32,6 +32,7 @@ for FILE in ${TARGET_FILES}; do
 
   redpen --result-format ${RESULT_FORMAT} \
          --limit ${ERROR_LIMIT} \
-         --conf tools/ja/redpen-conf.xml ${TARGET_MARKDOWN}
+         --conf tools/ja/redpen-conf.xml ${TARGET_MARKDOWN} \
+         2>tools/ja/redpen.log
 
 done
